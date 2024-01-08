a82e677fc9446175f10dabe413c89d00651d02cd
javokhir200 <javokhirganiboyev2004@gmail.com>
2024-01-08 18:48:51 +0500

    smth changed in group srvive

--- src/main/java/uz/pdp/univer/service/GroupService.java
+++ src/main/java/uz/pdp/univer/service/GroupService.java
@@ -1,4 +1,11 @@
 package uz.pdp.univer.service;
 
+import uz.pdp.univer.domain.Group;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public interface GroupService {
+    List<Group> GROUPS = new ArrayList<>();
+    String addGroup(Group group);
 }
