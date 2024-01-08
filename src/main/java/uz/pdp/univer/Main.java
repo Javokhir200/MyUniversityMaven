5d3deec820467d3f6043a99fc5a71cdfce49e73d
javokhir200 <javokhirganiboyev2004@gmail.com>
2024-01-08 13:32:20 +0500

    Salom

---
+++ src/main/java/uz/pdp/univer/Main.java
@@ -0,0 +1,12 @@
+package uz.pdp.univer;
+
+import uz.pdp.univer.domain.User;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        User us = new User()
+    }
+}
---
+++ src/main/java/uz/pdp/univer/domain/User.java
@@ -0,0 +1,22 @@
+package uz.pdp.univer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    static int sequence = 0;
+    {
+        sequence++;
+    }
+    private int userId = sequence;
+    private String fullName;
+    private String email;
+    private String username;
+    private String localPassword;
+    private int groupId;
+    private int facultyId;
+}
